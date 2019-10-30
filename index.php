1e89d91548affc56fb296817be2086e1de5abd08
Marcio Cotrim <marcio.cotrim@yahoo.com.br>
2019-10-30 11:29:20 -0300

    Produtos VS Categorias

--- index.php
+++ index.php
@@ -1,27 +1,20 @@
 <?php 
 
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
-use \Hcode\Page;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {
-    
-	//$sql = new Hcode\DB\Sql();
-
-	//$results = $sql->select("SELECT * FROM tb_users");
-
-	//echo json_encode($results);
-
-	$page = new Page();
-
-	$page->setTpl("index");
-
-});
+require_once("functions.php");
+require_once("site.php");
+require_once("admin.php");
+require_once("admin-users.php");
+require_once("admin-categories.php");
+require_once("admin-products.php");
 
 $app->run();
 
---
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Hcode;
+
+use Rain\Tpl;
+
+class Mailer 
+{
+
+	const USERNAME = "fmanhaes3@gmail.com";
+	const PASSWORD = "<?password?>";
+	const NAME_FROM = "Hcode Store";
+
+	private $mail;
+
+	public function __construct($toAddress, $toName, $subject, $tplName, $data = array())
+	{
+		$config = array
+		(
+		    
+		    "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/email/",
+		    "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+		    "debug"         => false
+		);
+
+		Tpl::configure($config);
+
+		$tpl = new Tpl;
+
+		foreach ($data as $key => $value) 
+		{
+			$tpl->assign($key, $value);
+		}
+
+		$html = $tpl->draw($tplName, true);
+
+		//Create a new PHPMailer instance
+		$this->mail = new \PHPMailer;
+
+		//Tell PHPMailer to use SMTP
+		$this->mail->isSMTP();
+
+		//Enable SMTP debugging
+		// SMTP::DEBUG_OFF = off (for production use)
+		// SMTP::DEBUG_CLIENT = client messages
+		// SMTP::DEBUG_SERVER = client and server messages
+		$this->mail->SMTPDebug = 0;
+
+		//Ask for HTML-friendly debug output
+		$this->mail->Debugoutput = 'html';
+
+		//Set the hostname of the mail server
+		$this->mail->Host = 'smtp.gmail.com';
+		// use
+		// $this->mail->Host = gethostbyname('smtp.gmail.com');
+		// if your network does not support SMTP over IPv6
+
+		//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+		$this->mail->Port = 587;
+
+		//Set the encryption mechanism to use - STARTTLS or SMTPS
+		$this->mail->SMTPSecure = 'tls';
+
+		//Whether to use SMTP authentication
+		$this->mail->SMTPAuth = true;
+
+		//Username to use for SMTP authentication - use full email address for gmail
+		$this->mail->Username = Mailer::USERNAME;
+		//$this->mail->Username = 'fmanhaes3@gmail.com';'Cotrim';
+
+		//Password to use for SMTP authentication
+		$this->mail->Password = Mailer::PASSWORD;
+
+		//Set who the message is to be sent from
+		$this->mail->setFrom(Mailer::USERNAME, Mailer::NAME_FROM);
+
+		//Set an alternative reply-to address
+		//$this->mail->addReplyTo('replyto@example.com', 'First Last');
+
+		//Set who the message is to be sent to
+		$this->mail->addAddress($toAddress, $toName);
+
+		//Set the subject line
+		$this->mail->Subject = $subject;
+
+		//Read an HTML message body from an external file, convert referenced images to embedded,
+		//convert HTML into a basic plain-text alternative body
+		//$this->mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+		$this->mail->msgHTML($html);
+
+		//Replace the plain text body with one created manually
+		$this->mail->AltBody = 'Essa é uma mensagem de teste via script.';
+
+		//Attach an image file
+		//$this->mail->addAttachment('images/phpmailer_mini.png');
+
+	}
+
+	public function send()
+	{
+		return $this->mail->send();
+	}
+
+}
+
+?>
