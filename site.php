d5c48f13e684575cae47bf76d8ed3cbf03db72c7
Marcio Cotrim <marcio.cotrim@yahoo.com.br>
2019-10-31 23:07:30 -0300

    Calculo de Frete

--- site.php
+++ site.php
@@ -71,7 +71,8 @@ $app->get("/cart", function(){
 
 	$page->setTpl("cart", [
 		'cart'=>$cart->getValues(),
-		'products'=>$cart->getProducts()
+		'products'=>$cart->getProducts(),
+		'error'=>Cart::getMsgError()
 	]);
 });
 
@@ -121,6 +122,18 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
 	$cart->removeProduct($product, true);
 
 	header("Location: /cart");
+	exit;
+
+});
+
+$app->post("/cart/freight", function(){
+
+	$cart = Cart::getFromSession();
+
+	$cart->setFreight($_POST['zipcode']);
+
+	header("Location: /cart");
+
 	exit;
 
 });
